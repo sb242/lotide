35e8f300f8f16bdc4ae62a6313cb7a79e2941769
Spencer Bethel <105334105+sb242@users.noreply.github.com>
2022-08-22 15:22:48 -0600

    updated text output to use string literals instead of concatenation

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("🟢 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🔴 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
