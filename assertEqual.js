3ec81b3d9cefc8108d98b566d1bb846b9299f8b1
Spencer Bethel <105334105+sb242@users.noreply.github.com>
2022-08-22 15:48:21 -0600

    added tail.js and updated head.js and assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
@@ -7,6 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(1, 0);
