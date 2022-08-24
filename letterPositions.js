a088331f8ae5c820532386a59c24d1c5ff5e8086
Spencer Bethel <105334105+sb242@users.noreply.github.com>
2022-08-24 15:25:53 -0600

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const eqArrays = function(array1, array2) { 
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  if (array1 === null || array2 === null) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`🟢 The arrays match: ${array1} === ${array2}`);
+  } else {
+    console.log(`🔴 The arrays do not match: ${array1} !== ${array2}`);
+  }
+};
+
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for(let i = 0; i < sentence.length; i++) {
+    if(sentence[i] !== ' ') { //skips over white spaces
+      if(results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+}
+
+console.log(letterPositions("Hello Jane!!"));
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
