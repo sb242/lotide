113fc1a88da7ea040c577205505eef1bed866493
Spencer Bethel <105334105+sb242@users.noreply.github.com>
2022-08-25 15:49:11 -0600

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, callback) => {
+  for(item in object) {
+    if(callback(object[item])) {
+      return item;
+    }
+  }
+  return undefined;
+}
+
+let key = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(key, 'noma');
